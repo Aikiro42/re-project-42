fcb461585296afc935e858d94405f733f042d23b
Aikiro42 <hiroku042@gmail.com>
2024-08-13 18:13:06 +0800

    feat: add initial implementation
    - added some sort of boilerplate code for the ability, weapon and config

---
+++ items/active/weapons/melee/abilities/project42neo/project42neo.lua
@@ -0,0 +1,11 @@
+require "/items/active/weapons/melee/project42neo-meleeweapon.lua"
+
+Project42Neo = WeaponAbility:new()
+
+function Project42Neo:init()
+
+end
+
+function Project42Neo:update(dt, fireMode, shiftHeld)
+
+end
